c267ad8090d3165c7c75550ca266539dbc6871cd
GVignesh-SD <gvignesh380980@gmail.com>
2024-03-18 06:02:29 +0530

    completed the home page, sign in page and sign up page and added the add to wishlist functionality

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Borcelle Store",
+  description: "Next.js Borcelle Ecommerce Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ClerkProvider>
+          <Navbar />
+          {children}
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
