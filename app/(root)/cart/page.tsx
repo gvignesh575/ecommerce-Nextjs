c4b065ae02b2c458e755079d5dee5c8707ba97eb
GVignesh-SD <gvignesh380980@gmail.com>
2024-03-19 01:58:02 +0530

    completed the cart page functionality

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,22 +1,31 @@
+"use client";
 import useCart from "@/lib/hooks/useCart";
+import { MinusCircle, PlusCircle, Trash } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
 const Cart = () => {
   const cart = useCart();
 
+  const total = cart.cartItems.reduce(
+    (acc, cartItem) => acc + cartItem.item.price * cartItem.quantity,
+    0
+  );
+
+  const totalRounded = parseFloat(total.toFixed(2));
+
   return (
-    <div className="flex gap-20 py-16 px-10">
-      <div className="w-2/3">
-        <p className="text-heading-bold">Shopping Cart</p>
-        <hr />
+    <div className="flex gap-20 py-16 px-10 max-lg:flex-col">
+      <div className="w-2/3 max-lg:w-full">
+        <p className="text-heading3-bold">Shopping Cart</p>
+        <hr className="my-6" />
 
         {cart.cartItems.length === 0 ? (
-          <p className="text-body-bold">No Item in cart</p>
+          <p className="text-body-bold text-center my-10">No Item in cart</p>
         ) : (
           <div>
             {cart.cartItems.map((cartItem) => (
-              <div className="flex hover:bg-grey-1 px-6 py-5 justify-center items-center">
+              <div className="w-full justify-between flex max-sm:flex-col max-sm:gap-3 max-sm:items-start hover:bg-grey-1 px-6 py-5 items-center">
                 <div className="flex items-center">
                   <Image
                     src={cartItem.item.media[0]}
@@ -33,12 +42,48 @@ const Cart = () => {
                     {cartItem.size && (
                       <p className="text-small-medium">{cartItem.size}</p>
                     )}
+                    <p className="text-small-medium">${cartItem.item.price}</p>
                   </div>
                 </div>
+
+                <div className="flex gap-4 items-center">
+                  <MinusCircle
+                    className="hover:text-red-1 cursor-pointer"
+                    onClick={() => cart.decreaseQuantity(cartItem.item._id)}
+                  />
+                  <p className="text-body-bold">{cartItem.quantity}</p>
+                  <PlusCircle
+                    className="hover:text-red-1 cursor-pointer"
+                    onClick={() => cart.increaseQuantity(cartItem.item._id)}
+                  />
+                </div>
+
+                <Trash
+                  className="hover:text-red-1 cursor-pointer"
+                  onClick={() => cart.removeItem(cartItem.item._id)}
+                />
               </div>
             ))}
           </div>
         )}
+      </div>
+      <div className="w-1/3 max-lg:w-full flex flex-col gap-8 bg-grey-1 rounded-lg px-4 py-5">
+        <p className="text-heading4-bold pb-4">
+          Summary
+          <span>
+            {`(${cart.cartItems.length} ${
+              cart.cartItems.length > 1 ? "items" : "item"
+            })`}
+          </span>
+        </p>
+        <div className="flex justify-between text-body-semibold">
+          <span>Total Amount</span>
+          <span>${totalRounded}</span>
+        </div>
+
+        <button className="border rounded-lg text-body-bold bg-white py-3 w-full hover:bg-black hover:text-white">
+          Proceed to Checkout
+        </button>
       </div>
     </div>
   );
